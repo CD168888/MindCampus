0f39f05bcfdea362995a9153cacf09027dec065b
小渡吖~ <yiyanwenxin@gmail.com>
2025-09-25 20:37:18 +0800

    修复

--- mc-ui/MindCampus-Web/src/views/counselor/counselorinfo/index.vue
+++ mc-ui/MindCampus-Web/src/views/counselor/counselorinfo/index.vue
@@ -27,19 +27,19 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button type="primary" plain icon="Plus" @click="handleAdd"
-          v-hasPermi="['counselor:counselorinfo:add']">新增</el-button>
+                   v-hasPermi="['counselor:counselorinfo:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
-          v-hasPermi="['counselor:counselorinfo:edit']">修改</el-button>
+                   v-hasPermi="['counselor:counselorinfo:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
-          v-hasPermi="['counselor:counselorinfo:remove']">删除</el-button>
+                   v-hasPermi="['counselor:counselorinfo:remove']">删除</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button type="warning" plain icon="Download" @click="handleExport"
-          v-hasPermi="['counselor:counselorinfo:export']">导出</el-button>
+                   v-hasPermi="['counselor:counselorinfo:export']">导出</el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
@@ -60,15 +60,15 @@
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
           <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
-            v-hasPermi="['counselor:counselorinfo:edit']">修改</el-button>
+                     v-hasPermi="['counselor:counselorinfo:edit']">修改</el-button>
           <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
-            v-hasPermi="['counselor:counselorinfo:remove']">删除</el-button>
+                     v-hasPermi="['counselor:counselorinfo:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize" @pagination="getList" />
+                v-model:limit="queryParams.pageSize" @pagination="getList" />
 
     <!-- 添加或修改辅导员管理对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
@@ -78,7 +78,7 @@
         </el-form-item>
         <el-form-item label="用户昵称" prop="userNickName">
           <el-select v-model="form.userId" placeholder="请选择用户昵称" :disabled="isEditMode" clearable
-            @change="handleUserChange">
+                     @change="handleUserChange">
             <el-option v-for="user in unbindUsers" :key="user.userId" :label="user.nickName" :value="user.userId" />
           </el-select>
         </el-form-item>
@@ -97,7 +97,7 @@
         <el-form-item label="状态" prop="status">
           <el-select v-model="form.status" placeholder="请选择状态">
             <el-option v-for="dict in counselor_status" :key="dict.value" :label="dict.label"
-              :value="dict.value"></el-option>
+                       :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
       </el-form>
