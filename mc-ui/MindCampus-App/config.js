d4d6bba691d0ab2780dc3d6badde4ab2834a6878
小渡吖~ <yiyanwenxin@gmail.com>
2026-05-07 00:06:14 +0800

    修改打包配置

--- mc-ui/MindCampus-App/config.js
+++ mc-ui/MindCampus-App/config.js
@@ -1,20 +1,11 @@
-// 应用全局配置
 module.exports = {
-  // baseUrl: 'https://vue.ruoyi.vip/prod-api',
-  baseUrl: 'http://localhost:8080',
-  // 应用信息
+  baseUrl: '/dev-api',
   appInfo: {
-    // 应用名称
     name: "MindCampus",
-    // 应用版本
     version: "1.2.0",
-    // 应用logo
     logo: "/static/logo.png",
-    // 官方网站
     site_url: "https://mindcampus.com",
-    // 应用简介
     description: "专业的大学生心理健康服务平台",
-    // 政策协议
     agreements: [{
         title: "隐私政策",
         url: "https://mindcampus.com/privacy.html"
